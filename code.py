7ec14a38460a5dd795f9c6078bb7f407f18afc02
Maria do Carmo <rodrigobissomachado@gmail.com>
2018-11-14 17:25:03 -0200

    leitura dos arquivos

--- code.py
+++ code.py
@@ -1,3 +1,31 @@
+import sys
+
+def help():
+	if len(sys.argv) < 3:
+		print("Uso: pyhton "+sys.argv[0]+" <arq_mem_prin> <arq_mem_cache>")
+
+if __name__ == '__main__':
 #dram 32 posicoes
 #cash 8 posicoes
-if __name__ == '__main__':
+	
+	#help()
+	dram = []
+	cache = ['0' for i in range(8)]
+	buffer_entrada = []
+	print(dram)
+	#"""
+	with open(sys.argv[1], 'r') as f:
+		for i in range(32):
+			dram.append(f.readline().replace('\n',''))
+	
+	with open(sys.argv[2], 'r') as f:
+		while True:
+			line = f.readline().replace('\n','')
+			if line == "":
+				break
+
+			buffer_entrada.append(line)
+
+
+	print(dram)
+	print(buffer_entrada)
